ef253dd40a3e9142e3c7c9221bb632f5087e4ccc
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-07-20 10:50:46 +1000

    Vue exercise 1 done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import "./styles.css";
 
 let vue = new Vue({
-  el:'#app',
-  data:{
-    title:"Hello Wolrd",
-    link:"http://google.com"
+  el: "#app",
+  data: {
+    title: "Hello Wolrd",
+    link: "http://google.com",
+    rawHTML: '<a href="http://google.com">Google</a>'
   },
-  methods:{
-    sayHello: function () {return this.title}
+  methods: {
+    sayHello: function() {
+      this.title = "Hey";
+      return this.title;
+    }
+  }
+});
+
+let exercise = new Vue({
+  el: "#exercise",
+  data: {
+    name: "Stanley",
+    age: 88,
+    img:
+      "https://www.rspcapetinsurance.org.au/RSPCA/media/Images/Hero/cat-insurance-hero.jpg"
+  },
+  methods: {
+    randomFloat: function() {
+      let number = (Math.random() * (1 - 0) + 0).toFixed(4);
+      return number;
+    }
   }
 });
