2dba171089a262733ed0a8d8b3e92c7479862f8b
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-07-23 09:52:23 +1000

    Looping through list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./styles.css";
 let vue = new Vue({
   el: "#app",
   data: {
-    show: true
+    ingredients: ["meat", "fruit", "cookies"],
+    persons: [
+      { name: "Max", age: 27, color: "red" },
+      { name: "Anna", age: "unknown", color: "blue" }
+    ]
   }
 });
