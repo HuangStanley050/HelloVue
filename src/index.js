26af88117c1e53905fc2f08f7511ad7443c637c4
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-07-22 10:11:58 +1000

    computed Vue

--- src/index.js
+++ src/index.js
@@ -3,54 +3,69 @@ import "./styles.css";
 let vue = new Vue({
   el: "#app",
   data: {
-    title: "Hello Wolrd",
-    link: "http://google.com",
-    rawHTML: '<a href="http://google.com">Google</a>'
-  },
-  methods: {
-    sayHello: function() {
-      this.title = "Hey";
-      return this.title;
-    }
-  }
-});
-
-let vEvent = new Vue({
-  el: "#app2",
-  data: {
+    name: "Stan",
     counter: 0,
-    x: 0,
-    y: 0
+    secoundCounter: 0,
+    result: ""
   },
-  methods: {
-    increase: function(number, event) {
-      this.counter += number;
-    },
-    updateCoordinate: function(e) {
-      this.x = e.clientX;
-      this.y = e.clientY;
-    },
-    dummy: function(e) {
-      e.stopPropagation();
-    },
-    alertMe: function() {
-      alert("You have entered");
+  computed: {
+    output: function() {
+      return this.counter > 5 ? "Greater 5" : "Smaller than 5";
     }
   }
 });
 
-let exercise = new Vue({
-  el: "#exercise",
-  data: {
-    name: "Stanley",
-    age: 88,
-    img:
-      "https://www.rspcapetinsurance.org.au/RSPCA/media/Images/Hero/cat-insurance-hero.jpg"
-  },
-  methods: {
-    randomFloat: function() {
-      let number = (Math.random() * (1 - 0) + 0).toFixed(4);
-      return number;
-    }
-  }
-});
+// let exercise2 = new Vue({
+//   el: "#exercise2",
+//   data: {
+//     value: null
+//   },
+//   methods: {
+//     showAlert: function() {
+//       alert("You click the button");
+//     },
+//     storeValue: function(e) {
+//       this.value = e.target.value;
+//     }
+//   }
+// });
+
+// let vEvent = new Vue({
+//   el: "#app2",
+//   data: {
+//     counter: 0,
+//     x: 0,
+//     y: 0
+//   },
+//   methods: {
+//     increase: function(number, event) {
+//       this.counter += number;
+//     },
+//     updateCoordinate: function(e) {
+//       this.x = e.clientX;
+//       this.y = e.clientY;
+//     },
+//     dummy: function(e) {
+//       e.stopPropagation();
+//     },
+//     alertMe: function() {
+//       alert("You have entered");
+//     }
+//   }
+// });
+
+// let exercise = new Vue({
+//   el: "#exercise",
+//   data: {
+//     name: "Stanley",
+//     age: 88,
+//     img:
+//       "https://www.rspcapetinsurance.org.au/RSPCA/media/Images/Hero/cat-insurance-hero.jpg"
+//   },
+//   methods: {
+//     randomFloat: function() {
+//       let number = (Math.random() * (1 - 0) + 0).toFixed(4);
+//       return number;
+//     }
+//   }
+// });
