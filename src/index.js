e2193439b8c3ff0550da5aa93f99e6e73139c069
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-07-23 10:47:09 +1000

    Looping exercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import "./styles.css";
 
-let vue = new Vue({
-  el: "#app",
+// let vue = new Vue({
+//   el: "#app",
+//   data: {
+//     ingredients: ["meat", "fruit", "cookies"],
+//     persons: [
+//       { name: "Max", age: 27, color: "red" },
+//       { name: "Anna", age: "unknown", color: "blue" }
+//     ]
+//   }
+// });
+
+let exercise = new Vue({
+  el: "#exercise",
   data: {
-    ingredients: ["meat", "fruit", "cookies"],
-    persons: [
-      { name: "Max", age: 27, color: "red" },
-      { name: "Anna", age: "unknown", color: "blue" }
-    ]
+    show: true,
+    question2: ["meat", "fish", "burger"],
+    LOR: { title: "Lord of the Rings", author: "J.R.R. Tolkiens", books: "3" }
   }
 });
